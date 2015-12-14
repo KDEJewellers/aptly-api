122af7fffffadac2ea802192b7bd63ff0d4c7cb6
Harald Sitter <sitter@kde.org>
2015-12-14 13:39:59 +0100

    don't needlessly pass connections around on instances

--- lib/aptly/repository.rb
+++ lib/aptly/repository.rb
@@ -14,14 +14,14 @@ module Aptly
 
     # 404	repository with such name doesn’t exist
     # 409	repository can’t be dropped (reason in the message)
-    def delete(connection = @connection, **kwords)
+    def delete(**kwords)
       connection.send(:delete, "/repos/#{self.Name}",
                       query: kwords)
       # 409 #<HTTParty::Response:0x25381d8 parsed_response=[{"error"=>"unable to drop, local repo is published", "meta"=>"Operation aborted"}], @response=#<Net::HTTPConflict 409 Conflict readbody=true>, @headers={"content-type"=>["application/json; charset=utf-8"], "date"=>["Fri, 11 Dec 2015 11:28:39 GMT"], "content-length"=>["81"]}>
     end
 
     # 404	repository with such name doesn’t exist
-    def add_file(path, connection = @connection, **kwords)
+    def add_file(path, **kwords)
       kwords = kwords.map { |k, v| [k.to_s.capitalize, v] }.to_h
       response = connection.send(:post, "/repos/#{self.Name}/file/#{path}",
                                  query: kwords)
@@ -29,25 +29,25 @@ module Aptly
     end
 
     # Convenience wrapper around {Files.upload} and {#add_file}
-    def upload(files, connection = @connection)
+    def upload(files)
       prefix = "#{self.class.to_s.tr(':', '_')}-#{Socket.gethostname}-"
       directory = Dir::Tmpname.make_tmpname(prefix, nil)
       files = Files.upload(files, directory, connection)
-      files.each { |f| add_file(f, connection) }
+      files.each { |f| add_file(f) }
     ensure
       # FIXME: delete dir?
     end
 
     # Convenience wrapper around {Aptly.publish}
-    def publish(prefix, connection = @connection, **kwords)
-      Aptly.publish([{ Name: self.Name }], prefix, 'local', connection, kwords)
+    def publish(prefix, **kwords)
+      Aptly.publish([{ Name: self.Name }], prefix, 'local', kwords)
     end
 
-    def published?(connection = @connection)
-      !published_in(connection).empty?
+    def published?
+      !published_in.empty?
     end
 
-    def published_in(connection = @connection)
+    def published_in
       Aptly::PublishedRepository.list(connection).select do |pub|
         pub.Sources.each do |src|
           next false unless src.Name == self.Name

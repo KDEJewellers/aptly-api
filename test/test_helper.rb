782bbb75e26eb5dc3109366967e8ef4d58dc38fc
Harald Sitter <sitter@kde.org>
2016-03-14 10:22:53 +0100

    change require order to avoid circular requiring between mock and autorun

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,6 +11,6 @@ SimpleCov.start
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'aptly'
 
-require 'webmock/minitest'
-
 require 'minitest/autorun'
+
+require 'webmock/minitest'

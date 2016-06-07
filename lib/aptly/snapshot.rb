d2911301150acc783d2d7d507e0f9dc0a0cdd937
Rohan Garg <rohan@garg.io>
2016-06-07 12:12:00 +0200

    Add a published_in method to Aptly::Snapshot like we have in Aptly::Repository

--- lib/aptly/snapshot.rb
+++ lib/aptly/snapshot.rb
@@ -50,6 +50,23 @@ module Aptly
       Aptly.publish([{ Name: self.Name }], prefix, 'snapshot', kwords)
     end
 
+    # Lists all PublishedRepositories self is published in. Namely self must
+    # be a source of the published repository in order for it to appear here.
+    # This method always returns an array of affected published repositories.
+    # If you use this method with a block it will additionally yield each
+    # published repository that would appear in the array, making it a shorthand
+    # for {Array#each}.
+    # @yieldparam pub [PublishedRepository]
+    # @return [Array<PublishedRepository>]
+    def published_in
+      Aptly::PublishedRepository.list(connection).select do |pub|
+        next false unless pub.Sources.any? do |src|
+          src.Name == self.Name
+        end
+        yield pub if block_given?
+        true
+      end
+    end
 
     class << self
       # List all known snapshots.

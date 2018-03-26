5abebfea73eaec2ae11fb33baa5b89a86c13288a
Harald Sitter <sitter@kde.org>
2018-03-26 12:23:56 +0200

    fix some additional doc warnings
    somehow referncing Array#each doesn't properly resolve and cause a warning,
    drop it as reference

--- lib/aptly/publishable.rb
+++ lib/aptly/publishable.rb
@@ -1,11 +1,13 @@
 module Aptly
+  # Abstract "type" of all publishable entities. Publishable entities
+  # are everything that can act as Source for a PublishedRepository.
   module Publishable
     # Lists all PublishedRepositories self is published in. Namely self must
     # be a source of the published repository in order for it to appear here.
     # This method always returns an array of affected published repositories.
     # If you use this method with a block it will additionally yield each
     # published repository that would appear in the array, making it a shorthand
-    # for {Array#each}.
+    # for Array#each.
     # @yieldparam pub [PublishedRepository]
     # @return [Array<PublishedRepository>]
     def published_in
--- lib/aptly/version.rb
+++ lib/aptly/version.rb
@@ -1,4 +1,5 @@
 # Aptly API
 module Aptly
+  # The loaded version of the gem.
   VERSION = '0.10.0'.freeze
 end
